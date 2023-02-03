be0fafba9269b718c502b40f4307eff68f557e14
张鹏程(时速云) <zhang.pengcheng3@21vianet.com>
2023-02-03 14:53:33 +0800

    fix: [PAASC-8002] Optimize page style and config

--- config/config.ts
+++ config/config.ts
@@ -30,12 +30,60 @@ const getLastCommitHash = () => {
 const site = 'tenxcloud.com';
 const bannerFlag = `@Licensed Materials - Property of ${site}`;
 const banner = `${bannerFlag}
-(C) Copyright 2022~2023 ${site}. All Rights Reserved.
+(C) Copyright 2023~2024 ${site}. All Rights Reserved.
 @date ${Date.now()}
 @hash ${getLastCommitHash()}
 http://${site}`;
 
 export default defineConfig({
+  /**
+   * @name 开启 hash 模式
+   * @description 让 build 之后的产物包含 hash 后缀。通常用于增量发布和避免浏览器加载缓存。
+   * @doc https://umijs.org/docs/api/config#hash
+   */
+  hash: true,
+  /**
+   * @name 快速热更新配置
+   * @description 一个不错的热更新组件，更新时可以保留 state
+   */
+  fastRefresh: true,
+  /**
+   * @name moment2dayjs 插件
+   * @description 将项目中的 moment 替换为 dayjs
+   * @doc https://umijs.org/docs/max/moment2dayjs
+   */
+  moment2dayjs: {
+    preset: 'antd',
+    plugins: ['duration'],
+  },
+  /**
+   * @name antd 插件
+   * @description 内置了 babel import 插件
+   * @doc https://umijs.org/docs/max/antd#antd
+   */
+  antd: {
+    import: false,
+    theme,
+  },
+  /**
+   * @name 兼容性设置
+   * @description 设置 ie11 不一定完美兼容，需要检查自己使用的所有依赖
+   * @doc https://umijs.org/docs/api/config#targets
+   */
+  targets: {
+    chrome: 49,
+    firefox: 64,
+    safari: 10,
+    edge: 13,
+    ios: 10,
+    // ie: 11,
+  },
+  /**
+   * @name moment 的国际化配置
+   * @description 如果对国际化没有要求，打开之后能减少js的包大小
+   * @doc https://umijs.org/docs/api/config#ignoremomentlocale
+   */
+  ignoreMomentLocale: true,
   historyWithQuery: {},
   history: {
     type: 'browser',
@@ -43,11 +91,8 @@ export default defineConfig({
   codeSplitting: {
     jsStrategy: 'granularChunks',
   },
-  antd: {
-    import: false,
-    theme,
-  },
   layout: {},
+  routes,
   lessLoader: {
     modifyVars: {
       '@ant-prefix': 'ant',
@@ -63,16 +108,6 @@ export default defineConfig({
       },
     },
   },
-  routes,
-  targets: {
-    chrome: 49,
-    firefox: 64,
-    safari: 10,
-    edge: 13,
-    ios: 10,
-    // ie: 11,
-  },
-  ignoreMomentLocale: true,
   favicons: ['/favicon.ico'],
   base: '/',
   publicPath: '/',
